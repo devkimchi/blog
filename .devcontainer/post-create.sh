1987afdceb282d145a51893938159f588c23a224
Justin Yoo <justin.yoojh@gmail.com>
2022-10-02 08:58:13 +0000

    Update devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,7 +4,7 @@ sudo apt-get update && \
 
 ## Configure git
 git config --global pull.rebase false
-git config --global core.autocrlf false
+git config --global core.autocrlf input
 
 ## Enable local HTTPS for .NET
 dotnet dev-certs https --trust
@@ -12,6 +12,15 @@ dotnet dev-certs https --trust
 ## Install Gridsome
 npm install --global @gridsome/cli
 # npm install
+
+## CaskaydiaCove Nerd Font
+# Uncomment the below to install the CaskaydiaCove Nerd Font
+mkdir $HOME/.local
+mkdir $HOME/.local/share
+mkdir $HOME/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip
+unzip CascadiaCode.zip -d $HOME/.local/share/fonts
+rm CascadiaCode.zip
 
 ## AZURE CLI EXTENSIONS ##
 # Uncomment the below to install Azure CLI extensions
@@ -62,3 +71,19 @@ npm install --global @gridsome/cli
 # # To customize prompt, run 'p10k configure' or edit ~/.p10k.zsh.
 # [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 # " >> $HOME/.zshrc
+
+## OH-MY-POSH ##
+# Uncomment the below to install oh-my-posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+## OH-MY-POSH - POWERLEVEL10K SETTINGS ##
+# Uncomment the below to update the oh-my-posh settings without dotfiles integration
+# curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/p10k-with-clock.omp.json > $HOME/p10k-with-clock.omp.json
+# curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/p10k-without-clock.omp.json > $HOME/p10k-without-clock.omp.json
+# curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/switch-p10k-clock.ps1 > $HOME/switch-p10k-clock.ps1
+
+# mkdir $HOME/.config/powershell
+# curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/Microsoft.PowerShell_profile.ps1 > $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
+
+# cp $HOME/p10k-with-clock.omp.json $HOME/p10k.omp.json
