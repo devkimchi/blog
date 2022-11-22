f74ea5a8038c4f9f4b3ea277bb7024565174efcb
Justin Yoo <justin.yoojh@gmail.com>
2022-11-22 22:37:20 +0900

    Update RSS feed settings

--- gridsome.config.js
+++ gridsome.config.js
@@ -60,6 +60,8 @@ module.exports = {
       use: 'gridsome-plugin-rss',
       options: {
         contentTypeName: 'Post',
+        latest: true,
+        dateField: 'date',
         feedOptions: {
           title: 'Dev Kimchi',
           feed_url: 'https://devkimchi.com/feed.xml',
